7b81173fb7ccb9173ab554b70e1e32b7470510e1
isirode <isirode@outlook.com>
2022-11-29 19:02:11 +0100

    Adding limiters to draw either only inside a collider 2D or not inside a collider 2D

---
+++ Assets/Isirode/DrawingLines2D/Scripts/Limiter/NotInsideCollider2DLimiter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotInsideCollider2DLimiter : MonoBehaviour
+{
+    // WARNING : this will not work with a EdgeCollider2D or a CompositeCollider2D set as edge physics
+    // TODO : verify this in the Start method
+    public new Collider2D collider2D;
+
+    public LegacyInputController legacyInputController;
+
+    void Start()
+    {
+        if (collider2D == null)
+        {
+            collider2D = GetComponent<Collider2D>();
+        }
+        if (legacyInputController == null)
+        {
+            legacyInputController = GetComponent<LegacyInputController>();
+        }
+        if (legacyInputController != null)
+        {
+            legacyInputController.PointAdded += PointAdded;
+        }
+    }
+
+    private void PointAdded(List<Vector3> currentPoints, Vector3 newPoint)
+    {
+        if (collider2D.OverlapPoint(new Vector2(newPoint.x, newPoint.y)))
+        {
+            legacyInputController.FinishLine();
+        }
+    }
+}
